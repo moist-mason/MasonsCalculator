55efe3c869de25e631e08b3a9c9b801f47032eff
moist-mason <67809772+moist-mason@users.noreply.github.com>
2020-07-06 17:45:43 -0500

    Update MasonsCalculator.cs

--- MasonsCalculator/MasonsCalculator.cs
+++ MasonsCalculator/MasonsCalculator.cs
@@ -1,40 +1,47 @@
-﻿// Mason's Calculator v0.3.1
+﻿// Mason's Calculator v1.0-rc1
 // Written in Visual Studio 2019 using .NET Core 3.1
 using System;
 
 namespace MasonsCalculator
 {
     class MasonsCalculator
-    { 
+    {
+
         static void Main(string[] args)
         {
-            string function;
+            string mathFunc;
 
-            Console.WriteLine("Welcome to Mason's Calculator v0.3.1. Which of the following functions do you want to perform? Please type in all uppercase.\nARITHMETIC, TRIGONOMETRY, INVERSE TRIGONOMETRY, ABSOLUTE VALUES\nLOGARITHMS, NATURAL LOGARITHMS, SQUARE ROOTS");
-            function = Console.ReadLine();
+            Console.WriteLine("Welcome to Mason's Calculator RC1. Type in one of the following functions to perform or type 'Quit' to quit the program.\nArithmetic, Trigonometry, Inverse Trigonometry, Absolute Values, Logarithms, Natural Logarithms, Exponents, Square Roots");
+            mathFunc = Console.ReadLine();
 
-            switch (function)
+            switch (mathFunc)
             {
-                case "ARITHMETIC":
+                case "Arithmetic":
                     CalculatorArithmetic();
                     break;
-                case "TRIGONOMETRY":
+                case "Trigonometry":
                     CalculatorTrigonometry();
                     break;
-                case "INVERSE TRIGONOMETRY":
+                case "Inverse Trigonometry":
                     CalculatorInverseTrig();
                     break;
-                case "ABSOLUTE VALUES":
+                case "AbsoluteValues":
                     CalculatorAbsolute();
                     break;
-                case "LOGARITHMS":
+                case "Logarithms":
                     CalculatorLogarithms();
                     break;
-                case "NATURAL LOGARITHMS":
+                case "Natural Logarithms":
                     CalculatorNaturalLogs();
                     break;
-                case "SQUARE ROOTS":
+                case "Exponents":
+                    CalculatorExponents();
+                    break;
+                case "Square Roots":
                     CalculatorSquareRoots();
+                    break;
+                case "Quit":
+                    Environment.Exit(0);
                     break;
                 default:
                     Console.WriteLine("An invalid function was entered. Press any key to terminate the console.");
@@ -46,8 +53,8 @@ namespace MasonsCalculator
         {
             double arithNum1;
             double arithNum2;
-            string userOp;
-           
+            string arithOp;
+
             Console.WriteLine("First, enter a number.");
             arithNum1 = Convert.ToDouble(Console.ReadLine());
 
@@ -55,14 +62,14 @@ namespace MasonsCalculator
             arithNum2 = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("Finally, enter one of the following operators:\n+, -, *, /, %, ^");
-            userOp = Convert.ToString(Console.ReadLine());
+            arithOp = Convert.ToString(Console.ReadLine());
 
-            switch (userOp)
+            switch (arithOp)
             {
                 case "+":
                     Console.WriteLine(arithNum1 + arithNum2);
                     break;
-                case "-":;
+                case "-":
                     Console.WriteLine(arithNum1 - arithNum2);
                     break;
                 case "*":
@@ -73,9 +80,6 @@ namespace MasonsCalculator
                     break;
                 case "%":
                     Console.WriteLine(arithNum1 % arithNum2);
-                    break;
-                case "^":
-                    Console.WriteLine(Convert.ToInt32(arithNum1) ^ Convert.ToInt32(arithNum2));
                     break;
                 default:
                     Console.WriteLine("An invalid operator was entered. Press any key to terminate the console.");
@@ -156,11 +160,11 @@ namespace MasonsCalculator
                     break;
             }
         }
-       
+
         static void CalculatorAbsolute()
         {
             double absNum;
-            
+
             Console.WriteLine("Enter a number to find its absolute value.");
             absNum = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine(Math.Abs(absNum));
@@ -179,16 +183,28 @@ namespace MasonsCalculator
         {
             double naturalLogNum;
 
-            Console.WriteLine("Enter a number to find its natural logarithm");
+            Console.WriteLine("Enter a number to find its natural logarithm.");
             naturalLogNum = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine(Math.Log(naturalLogNum));
         }
 
+        static void CalculatorExponents()
+        {
+            int baseNum;
+            int powNum;
+
+            Console.WriteLine("Enter your base number.");
+            baseNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your power number.");
+            powNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(baseNum ^ powNum);
+        }
+       
         static void CalculatorSquareRoots()
         {
             double sqrtNum;
 
-            Console.WriteLine("Enter a number to find its square root");
+            Console.WriteLine("Enter a number to find its square root.");
             sqrtNum = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine(Math.Sqrt(sqrtNum));
         }
